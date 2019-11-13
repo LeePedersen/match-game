540513ffcc6bb1472a6489cb6e350eeee7803f8a
Student <example@example.com>
2019-11-12 16:44:58 -0800

    add basic js logic

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+function attachContactListeners() {
+  $("div.col-md-6").on("click", "img#square1", function() {
+    $("#square1Img").show();
+    $("#square1").hide();
+  });
+}
+
+$(document).ready(function() {
+  // $("img").toggle();
+  attachContactListeners();
+})
